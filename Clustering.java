e86d621ef9a4fc45218a321d9e222b9d5254b58d
guilherme-marcello <guilemosm@gmail.com>
2022-12-03 01:56:38 +0000

    Added Clustering class

---
+++ Clustering.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.util.HashMap;
+
+public class Clustering {
+
+    public interface partitionerFunction {
+        int classify(int x);
+    }
+
+    public static List<List<Integer>> findClusters(List<Integer> list, partitionerFunction function) {
+        HashMap<Integer,List<Integer>> clusters = new HashMap<>();
+        for (Integer value : list) {
+            int clusterId = function.classify(value);
+            clusters.putIfAbsent(clusterId, new LinkedList<Integer>());
+            clusters.get(clusterId).add(value);
+        }
+        return new LinkedList<>(clusters.values());
+    }
+}
