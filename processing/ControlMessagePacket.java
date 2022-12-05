80061613efd9591a4af8f06528730abd372d51a6
guilherme-marcello <guilemosm@gmail.com>
2022-12-05 23:42:05 +0000

    Added ControlMessage processing

---
+++ processing/ControlMessagePacket.java
@@ -0,0 +1,34 @@
+package processing;
+
+import trace.Capture;
+import trace.Packet;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+public class ControlMessagePacket {
+    private static boolean isNumeric(String str){
+        return str != null && str.matches("[0-9.]+");
+    }
+
+	public static void inspectICMP(Capture capture) {
+		List<Packet> packets = capture.getPackets();
+		int icmpCounter = 0;
+		Set<Integer> icmpTypes = new HashSet<>(); 
+		for (Packet packet : packets) {
+			String protocol = packet.getProtocol();
+			String icmpType = packet.getICMPtype();
+	
+			if (protocol.equals("ICMP") && isNumeric(icmpType)) {
+				icmpCounter++;
+                icmpTypes.add(Integer.valueOf(icmpType));
+            }	
+		}
+        System.out.println(
+            String.format(
+                "[BEGIN - OUTPUT]\nNumber of ICMP packets:\t%s\nFound ICMP types:\t%s\n[END - OUTPUT]",
+                icmpCounter, icmpTypes.toString()
+            )
+        );
+	}
+}
