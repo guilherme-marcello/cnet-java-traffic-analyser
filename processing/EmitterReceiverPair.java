4de96410caf8045c4f3eea3f0877345c8c5490b6
guilherme-marcello <guilemosm@gmail.com>
2022-12-05 23:21:53 +0000

    Added EmitterReceiverPair processing class

---
+++ processing/EmitterReceiverPair.java
@@ -0,0 +1,44 @@
+package processing;
+
+import trace.Capture;
+import trace.Packet;
+import util.Models;
+import java.util.List;
+
+public class EmitterReceiverPair {
+    private static String classifyAddress(String address) {
+        if (Models.isValidIPv4(address))
+            return "IPv4";
+        if (Models.isValidIPv6(address))
+            return "IPv6";
+        return "Hostname";
+    }
+
+    public static void inspectAddresses(Capture capture) {
+		List<Packet> packets = capture.getPackets();
+		int ipv4Counter = 0, ipv6Counter = 0, hostnameCounter = 0;
+		
+		for (Packet packet : packets) {
+			String sourceType = classifyAddress(packet.getSourceIP());
+			String destinationType = classifyAddress(packet.getDestinationIP());
+            if (sourceType.equals(destinationType)) {
+                if (sourceType.equals("IPv4"))
+                    ipv4Counter++;
+                else if (sourceType.equals("IPv6"))
+                    ipv6Counter++;
+                else if (sourceType.equals("Hostname"))
+                    hostnameCounter++;
+            }	
+		}
+        System.out.println(
+            String.format(
+                "[BEGIN - OUTPUT]\n" +
+                "Number of packets with receiver and sender using IPv4 addr.:\t%s\n" +
+                "Number of packets with receiver and sender using IPv6 addr.:\t%s\n" +
+                "Number of packets with receiver and sender using hostnames:\t%s"
+                + "\n[END - OUTPUT]",
+                ipv4Counter, ipv6Counter, hostnameCounter
+            )
+        );
+	}
+}
