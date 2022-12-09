7d5da696886f329ef0809b7dddb00594e179c78d
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2022-12-09 11:51:39 +0000

    Added Javadoc for EmitterReceiverPair

--- processing/EmitterReceiverPair.java
+++ processing/EmitterReceiverPair.java
@@ -6,8 +6,18 @@ import util.Models;
 import util.MenuUtils;
 import java.util.List;
 
+
+/**
+ * this class provides methods and functions to process a capture and get information about emitter-receiver pairs
+ */
 public class EmitterReceiverPair {
+    /**
+     * Given an address, verify if it is IPv4, IPv5 or Hostname
+     * @param address	address to evaluate
+     * @return "IPv4", "IPv6" or "Hostname"
+     */
     private static String classifyAddress(String address) {
+        // classify address by applying Models with Pattern validation
         if (Models.isValidIPv4(address))
             return "IPv4";
         if (Models.isValidIPv6(address))
@@ -15,14 +25,21 @@ public class EmitterReceiverPair {
         return "Hostname";
     }
 
+    /**
+     * Given a capture, count number of emitter-receiver pairs using both IPv4, IPv6 or hostnames
+     * @param capture	Capture to evaluate
+     */
     public static void inspectAddresses(Capture capture) {
-		List<Packet> packets = capture.getPackets();
-		int ipv4Counter = 0, ipv6Counter = 0, hostnameCounter = 0;
-		
-		for (Packet packet : packets) {
-			String sourceType = classifyAddress(packet.getSourceIP());
-			String destinationType = classifyAddress(packet.getDestinationIP());
+        List<Packet> packets = capture.getPackets();
+        int ipv4Counter = 0, ipv6Counter = 0, hostnameCounter = 0;
+	// start counting from 0
+        for (Packet packet : packets) {
+            // for each packet, classify source IP and destination IP
+            String sourceType = classifyAddress(packet.getSourceIP());
+            String destinationType = classifyAddress(packet.getDestinationIP());
+            // if types are equal
             if (sourceType.equals(destinationType)) {
+                // increment respective counter
                 if (sourceType.equals("IPv4"))
                     ipv4Counter++;
                 else if (sourceType.equals("IPv6"))
@@ -30,7 +47,8 @@ public class EmitterReceiverPair {
                 else if (sourceType.equals("Hostname"))
                     hostnameCounter++;
             }	
-		}
+        }
+        // show module output: number of emmiter-receiver using both IPv4, IPv6 and hostnames
         MenuUtils.showOutput(
             String.format(
                 "Number of packets with receiver and sender using IPv4 addr.:\t%s\n" +
