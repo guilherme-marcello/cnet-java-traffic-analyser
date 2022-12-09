6db19f86b0e010c71726112c2ee4d5077e88f70a
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2022-12-09 11:45:00 +0000

    Added Javadoc for ControlMessagePacket.java

--- processing/ControlMessagePacket.java
+++ processing/ControlMessagePacket.java
@@ -7,24 +7,39 @@ import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
 
+
+/**
+ * this class provides methods and functions to process a capture and get information about Internet Control Messages
+ */
 public class ControlMessagePacket {
+    /**
+     * Given a string, verify if it is numeric
+     * @param str	String to evaluate
+     * @return logic value of "str is numeric"
+     */
     private static boolean isNumeric(String str){
         return str != null && str.matches("[0-9.]+");
     }
 
-	public static void inspectICMP(Capture capture) {
-		List<Packet> packets = capture.getPackets();
-		int icmpCounter = 0;
-		Set<Integer> icmpTypes = new HashSet<>(); 
-		for (Packet packet : packets) {
-			String protocol = packet.getProtocol();
-			String icmpType = packet.getICMPtype();
-	
-			if (isNumeric(icmpType) && (protocol.equals("ICMP") || protocol.equals("ICMPv6"))) {
-				icmpCounter++;
+    /**
+     * Given a capture, count number of ICMP packets and found ICMP types
+     * @param capture	Capture to evaluate
+     */
+    public static void inspectICMP(Capture capture) {
+        List<Packet> packets = capture.getPackets();
+        int icmpCounter = 0; // start conunting from 0
+        Set<Integer> icmpTypes = new HashSet<>(); 
+        for (Packet packet : packets) {
+            // for each packet, get protocol and icmp type.
+            String protocol = packet.getProtocol();
+            String icmpType = packet.getICMPtype();  
+            // if icmpType is numeric and protocol is ICMP, increment count and add found type to a Set
+            if (isNumeric(icmpType) && (protocol.equals("ICMP") || protocol.equals("ICMPv6"))) {
+                icmpCounter++;
                 icmpTypes.add(Integer.valueOf(icmpType));
-            }	
-		}
+             }	
+        }
+	// show module output: count of icmp packets and set of found icmp types
         MenuUtils.showOutput(
             String.format(
                 "Number of ICMP packets:\t%s\nFound ICMP types:\t%s",
