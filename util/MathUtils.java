810a1264fa6b188d6763eb54070128274eb84f5c
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2022-12-09 10:46:18 +0000

    Added Javadoc to MathUtils

--- util/MathUtils.java
+++ util/MathUtils.java
@@ -1,5 +1,13 @@
 package util;
+/**
+ * this class provides methods to extend Java default math operations
+ */
 public class MathUtils {
+    /**
+     * Given an integer x, return the binary logarithm of this number
+     * @param x value to be evaluated
+     * @return integer binary logarithm of x
+     */
     public static int log2(int x) {
         return (int) (Math.log(x) / Math.log(2));
     }
