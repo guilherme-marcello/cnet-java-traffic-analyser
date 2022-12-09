8ae20e6f35c9e4847585e75d057fcd0d1743a32c
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2022-12-09 11:17:41 +0000

    Added Javadoc for TCPFlag

--- trace/TCPFlag.java
+++ trace/TCPFlag.java
@@ -1,5 +1,8 @@
 package trace;
 
+/**
+ * this enum represent possible TCP flags
+ */
 public enum TCPFlag {
     SYN("0x002"),
     SYNACK("0x012"),
@@ -7,12 +10,23 @@ public enum TCPFlag {
     FIN("0x001"),
     UNNAMED("unnamed");
     
+    /**
+     * Build TCPFlag with its hexadecimal representation
+     * @param hexValue  hexadecimal representation of flag
+     */
     private final String hexValue;
     private TCPFlag(String hexValue) {
         this.hexValue = hexValue;
     }
-
+    
+    /**
+     * Given an hexadecimal representation of a flag, return TCPFlag
+     * @param hexValue  hexadecimal representation of flag
+     * @return TCPFlag of given hex value
+     */
     public static TCPFlag parseHex(String hexValue) {
+        // verify if hexValue is equal to correct representation
+        // return enum for each case; return UNNAMED otherwise
         if (hexValue.equals("0x002")) return SYN;
         if (hexValue.equals("0x012")) return SYNACK; 
         if (hexValue.equals("0x010")) return ACK; 
