09156ca708b4f4aaba74812a9746323667ef2822
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2022-12-09 10:43:14 +0000

    Added Javador for FilesUtil

--- util/FilesUtil.java
+++ util/FilesUtil.java
@@ -7,12 +7,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * this class provides methods to read and parse traces
+ */
 public class FilesUtil {
+    /**
+     * Read a trace file, parse content to a List of Packet and return a instance of Capture
+     * @param filename	Name of the source file
+     * @return a Capture instance with all Packet inside file
+     */
     public static Capture readTrace(String filename) {
+	// create new Capture instance
         Capture capture = new Capture();
 		try (Scanner scanner = new Scanner(new File(filename));) {
             scanner.nextLine();
 			while (scanner.hasNextLine()) {
+			// parse each line to a Packet and add Packet to capture
                 capture.add(
                     new Packet(scanner.nextLine())
                 );
